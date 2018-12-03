6dff02bd77f78d91c0d90624353bc64067257e37
Becka <rebecca.lelew@gmail.com>
2018-12-03 13:49:26 +0000

    Tidies up some rubocop offenses

--- lib/account_history.rb
+++ lib/account_history.rb
@@ -7,27 +7,25 @@ class AccountHistory
   end
 
   def add_transaction(deposit, new_balance)
-    date = Time.now.strftime("%d/%m/%Y")
-    @transactions.push({date: date, deposit: deposit, new_balance: new_balance})
+    date = Time.now.strftime('%d/%m/%Y')
+    @transactions.push({ date: date, deposit: deposit, balance: new_balance })
   end
 
   def statement
     statement = statement_header
     @transactions.each do |t|
       line = [(t[:date])]
-      line += withdrawal?(t[:deposit]) ? ["", -t[:deposit]] : [t[:deposit], ""]
-      line << t[:new_balance]
+      line += withdrawal?(t[:deposit]) ? ['', -t[:deposit]] : [t[:deposit], '']
+      line << t[:balance]
       statement += "\n#{line.join(' || ')}"
     end
     return statement
   end
 
-
-
   private
 
   def statement_header
-    return "date || credit || debit || balance"
+    'date || credit || debit || balance'
   end
 
   def withdrawal?(amount)
