36e522f8dc2f569d139be0d7da3ed92c741bc797
Becka <rebecca.lelew@gmail.com>
2018-12-03 11:43:46 +0000

    Adds ability to deposit amount

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe 'Account' do
   it 'initializes with a balance of zero' do
     expect(@account.balance).to eq 0
   end
+
+  it 'allows deposits' do
+    @account.deposit(100)
+    expect(@account.balance).to eq 100
+  end
 end
