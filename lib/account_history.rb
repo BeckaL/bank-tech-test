505e7c7f3f7aeacb8b023f7fc09a59fcecee10b4
Becka <rebecca.lelew@gmail.com>
2018-12-03 15:43:19 +0000

    More refactoring of statement

--- lib/account_history.rb
+++ lib/account_history.rb
@@ -12,13 +12,12 @@ class AccountHistory
   end
 
   def statement
-    statement = statement_header
-    @transactions.each do |t, k, v|
+    statement = @transactions.map do |t|
       line = t.map { |k, v| v }
       line[1] < 0 ? line.insert(1, "") : line.insert(2, "")
-      statement += "\n#{line.join(' || ')}"
+      "\n#{line.join(' || ')}"
     end
-    return statement
+    return statement_header + statement.join('')
   end
 
   private
