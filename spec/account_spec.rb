e7d91ade417bf027bb19ec72c3f623ca472d1e46
Becka <rebecca.lelew@gmail.com>
2018-12-03 11:25:21 +0000

    Adds bank account class and balance attribute

---
+++ spec/account_spec.rb
@@ -0,0 +1,13 @@
+require './lib/account.rb'
+
+describe 'Account' do
+
+  before do
+    @account = Account.new
+  end
+
+
+  it 'initializes with a balance of zero' do
+    expect(@account.balance).to eq 0
+  end
+end
