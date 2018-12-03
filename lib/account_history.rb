74eae5511752bc7d90653def07a2c0c2918ed9a4
Becka <rebecca.lelew@gmail.com>
2018-12-03 13:32:14 +0000

    Adds statement printing and dates

--- lib/account_history.rb
+++ lib/account_history.rb
@@ -7,7 +7,36 @@ class AccountHistory
   end
 
   def add_transaction(deposit, new_balance)
-    @transactions.push([deposit, new_balance])
+    date = Time.now.strftime("%d/%m/%Y")
+    @transactions.push([date, deposit, new_balance])
+  end
+
+  def statement
+    st = statement_header
+    @transactions.each do |transaction|
+      line = []
+      line.push(transaction.first)
+      withdrawal_or_deposit = transaction[1]
+      if withdrawal_or_deposit < 0
+        line.push("")
+        line.push(withdrawal_or_deposit.abs)
+      else
+        line.push(withdrawal_or_deposit)
+        line.push("")
+      end
+      p line
+      line.push(transaction.last)
+      st += "\n#{line.join(' || ')}"
+    end
+    return st
+  end
+
+
+
+  private
+
+  def statement_header
+    return "date || credit || debit || balance"
   end
 
 end
